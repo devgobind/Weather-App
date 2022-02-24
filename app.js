9f9e83b348b16fce60917c2d4cceb9485da0dc2d
Dev <devgobind14@gmail.com>
2022-02-24 22:45:38 +0530

    Changes

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-const location_input = document.querySelector(".location-container");
+const location_input = document.querySelector(".location-text");
 const location_button = document.querySelector(".submit");
+const _weather = document.querySelector(".weather-container");
+const _image = document.getElementById("sky_img")
 let api;
-const city = "Kolkata"
+//Enter API Key
 const api_key = "ee1e7ca3c60d370a5f3c0300cffcb78f"
 function locationHandler() {
     if(navigator.geolocation)
-        loc = navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        loc = navigator.geolocation.getCurrentPosition(weather);
     else
         console.log('Geolocation is not present');
 }
 
-function onSuccess(position){
+function weather(position, city){
+    city = location_input.value;
+    _sky = "10d";
+    console.log(city);
     api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api_key}`
-    fetch(api).then(response => response.json()).then(result => console.log(result))
-
+    fetch(api).then(response => response.json()).then(result => {
+        const {id} = result.weather[0];
+        console.log(id);
+        _image.src = `http://openweathermap.org/img/wn/${_sky}@2x.png`;
+        console.log(result.weather);
+    })
+    if(_weather.classList.contains("hide")){
+        _weather.classList.toggle("hide");
+    }
     const {latitude, longitude} = position.coords;
     console.log(latitude, longitude, position.coords);
 }
 
-function onError(error){
-    infoTxt.innerText = error.message;
-    infoTxt.classList.add("error");
-}
-
-
-    location_button.addEventListener("click", locationHandler);
+   location_button.addEventListener("click", locationHandler);
